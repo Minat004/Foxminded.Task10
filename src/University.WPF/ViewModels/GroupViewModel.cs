a96495e3df50ae4ac9493d32b739f7c8a92a8a28
Minato <umi0n3@gmail.com>
2023-06-12 13:44:28 +0200

    groups

--- src/University.WPF/ViewModels/GroupViewModel.cs
+++ src/University.WPF/ViewModels/GroupViewModel.cs
@@ -1,10 +1,34 @@
-﻿using University.Core.Models;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using University.Core.Interfaces;
+using University.Core.Models;
 
 namespace University.WPF.ViewModels;
 
 public partial class GroupViewModel : UnitedEntityViewModel
 {
-    public GroupViewModel(Group group) : base(group.Id, group.Name)
+    private readonly IGroupService<Group> _groupService;
+    private readonly Group _group;
+
+    public GroupViewModel(IGroupService<Group> groupService, Group group) : base(group.Id, group.Name)
     {
+        _groupService = groupService;
+        _group = group;
+
+        LoadGroupsByCourseAsync().GetAwaiter();
+    }
+    
+    [ObservableProperty]
+    private ObservableCollection<StudentViewModel> studentsByGroupViews = new(new List<StudentViewModel>());
+    
+    private async Task LoadGroupsByCourseAsync()
+    {
+        var groups = await _groupService.GetGroupStudentsAsync(_group.Id);
+        var viewModels = groups.Select(student => new StudentViewModel(student)).ToList();
+
+        StudentsByGroupViews = new ObservableCollection<StudentViewModel>(viewModels);
     }
 }
