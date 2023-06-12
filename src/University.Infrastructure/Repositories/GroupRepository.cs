cc397ad7d3b14c6374b4a196df21605906c6dc3f
Minato <umi0n3@gmail.com>
2023-06-12 13:44:01 +0200

    context factory

--- src/University.Infrastructure/Repositories/GroupRepository.cs
+++ src/University.Infrastructure/Repositories/GroupRepository.cs
@@ -7,16 +7,18 @@ namespace University.Infrastructure.Repositories;
 
 public class GroupRepository : IGroupRepository<Group>
 {
-    private readonly UniversityDbContext _context;
+    private readonly IDbContextFactory<UniversityDbContext> _dbContextFactory;
 
-    public GroupRepository(UniversityDbContext context)
+    public GroupRepository(IDbContextFactory<UniversityDbContext> dbContextFactory)
     {
-        _context = context;
+        _dbContextFactory = dbContextFactory;
     }
     
     public async Task<IEnumerable<Group>> GetAllAsync()
     {
-        var groups = await _context.Groups
+        await using var context = await _dbContextFactory.CreateDbContextAsync();
+        
+        var groups = await context.Groups
             .Include(x => x.Course)
             .Include(x => x.Teacher)
             .ToListAsync();
@@ -26,17 +28,21 @@ public class GroupRepository : IGroupRepository<Group>
 
     public async Task<IEnumerable<Student>> GetGroupStudentsAsync(int groupId)
     {
-        return await _context.Students.Where(x => x.GroupId == groupId).ToListAsync();
+        await using var context = await _dbContextFactory.CreateDbContextAsync();
+        return await context.Students.Where(x => x.GroupId == groupId).ToListAsync();
     }
 
     public async Task<Teacher> GetTeacherOrDefaultAsync(int teacherId)
     {
-        return (await _context.Teachers.Where(x => x.Id == teacherId).FirstOrDefaultAsync())!;
+        await using var context = await _dbContextFactory.CreateDbContextAsync();
+        return (await context.Teachers.Where(x => x.Id == teacherId).FirstOrDefaultAsync())!;
     }
 
     public async Task UpdateAsync(Group item)
     {
-        var newGroup = _context.Groups.FirstOrDefault(x => x.Id == item.Id);
+        await using var context = await _dbContextFactory.CreateDbContextAsync();
+        
+        var newGroup = context.Groups.FirstOrDefault(x => x.Id == item.Id);
 
         if (newGroup is null)
         {
@@ -45,19 +51,23 @@ public class GroupRepository : IGroupRepository<Group>
         
         newGroup.Name = item.Name;
         
-        _context.Update(newGroup);
-        await _context.SaveChangesAsync();
+        context.Update(newGroup);
+        await context.SaveChangesAsync();
     }
 
     public async Task AddAsync(Group group)
     {
-        _context.Add(group);
-        await _context.SaveChangesAsync();
+        await using var context = await _dbContextFactory.CreateDbContextAsync();
+        
+        context.Add(group);
+        await context.SaveChangesAsync();
     }
 
     public async Task DeleteAsync(Group group)
     {
-        _context.Remove(group);
-        await _context.SaveChangesAsync();
+        await using var context = await _dbContextFactory.CreateDbContextAsync();
+        
+        context.Remove(group);
+        await context.SaveChangesAsync();
     }
 }
