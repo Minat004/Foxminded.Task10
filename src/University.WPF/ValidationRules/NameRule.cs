d79a8b43a09569dad61a8a771fcd738243f7ed03
Minato <umi0n3@gmail.com>
2023-06-22 14:43:31 +0200

    validation rule text only

--- src/University.WPF/ValidationRules/NameRule.cs
+++ src/University.WPF/ValidationRules/NameRule.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
 using System.Windows.Controls;
 
-namespace University.WPF.Validation;
+namespace University.WPF.ValidationRules;
 
 public class NameRule : ValidationRule
 {
     public NameRule()
     {
     }
+
+    public bool TextOnly { get; set; }
 
     public int Min { get; set; } = 1;
     
@@ -21,6 +23,11 @@ public class NameRule : ValidationRule
         if (name.Length < Min || name.Length > Max)
         {
             return new ValidationResult(false, $"The length of text must be in range: {Min}:{Max}");
+        }
+
+        if (TextOnly && !Regex.IsMatch(name, @"^[\p{L}\p{Mn}]+$"))
+        {
+            return new ValidationResult(false, $"The field is letter only.");
         }
         
         return ValidationResult.ValidResult;
